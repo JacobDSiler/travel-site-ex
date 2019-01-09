1c85e650691f45b3a22a50a72b31cf40bff134ca
Jacob <jacob.siler91@gmail.com>
2019-01-09 22:41:30 +0000

    Completed automatic sprite generation and use.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,9 +18,10 @@ gulp.task('watch', function() {
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
-return gulp.src('./app/temp/styles/style.css')
-.pipe(browserSync.stream(), browserSync.reload());
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
 });
