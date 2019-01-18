9f122dd8eff42236094eaa4430804fb16cad7f12
Jacob <jacob.siler91@gmail.com>
2019-01-18 21:22:01 +0000

    Completed desktop and mobile header styles.

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -103,6 +103,7 @@ var MobileMenu = function () {
   function MobileMenu() {
     _classCallCheck(this, MobileMenu);
 
+    this.siteHeader = (0, _jquery2.default)(".site-header");
     this.menuIcon = (0, _jquery2.default)(".site-header__menu-icon");
     this.menuContent = (0, _jquery2.default)(".site-header__menu-content");
     this.events();
@@ -111,12 +112,14 @@ var MobileMenu = function () {
   _createClass(MobileMenu, [{
     key: "events",
     value: function events() {
-      this.menuIcon.click(this.toggleTheMenu);
+      this.menuIcon.click(this.toggleTheMenu.bind(this));
     }
   }, {
     key: "toggleTheMenu",
     value: function toggleTheMenu() {
-      this.menuContent.toggleClass("site-header-content--is-visible");
+      this.menuContent.toggleClass("site-header__menu-content--is-visible");
+      this.siteHeader.toggleClass("site-header--is-expanded");
+      this.menuIcon.toggleClass("site-header__menu-icon--close-x");
     }
   }]);
 
